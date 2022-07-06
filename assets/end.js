34cce096b1d7c8996db0c19670cf5d25c19a7664
dhuw <hazlewood.dyl@gmail.com>
2022-07-05 21:23:52 -0700

    style.css and end.js update
    finishing up end page section of style.css. and started end.js

--- assets/end.js
+++ assets/end.js
@@ -1,1 +1,14 @@
-console.log('yo im linked')
+const username = $('#username')
+const saveScoreBtn = $('#save-score-btn')
+const finalScore = $('#final-score')
+const mostRecentScore = $('#most-recent-score')
+
+const highscores = JSON.parse(localStorage.getItem('highscores')) || []
+
+const MAX_HIGHSCORES = 1000
+
+finalScore.innerText = mostRecentScore
+//makes save btn work when the input has a value other then null
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value
+})
