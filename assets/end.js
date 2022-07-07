ef5db7e71d5b3f89f7bb4e204327623135b7b94a
dhuw <hazlewood.dyl@gmail.com>
2022-07-06 18:52:26 -0700

    Finished end.js adding highscores page update
    + highscores.css
    + highscores.js
    + highscores.html container

--- assets/end.js
+++ assets/end.js
@@ -12,3 +12,25 @@ finalScore.innerText = mostRecentScore
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value
 })
+
+saveHighscore = e = => {
+    e.preventDefault()
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    }
+
+    highscores.push(scores)
+
+    highscores.sort((a,b) => {
+        return b.score - a.score
+    })
+
+    highscores.splice(10)
+
+    localStorage.setItem('highscores', JSON.stringify(highscores))
+    window.location.assign('/')
+}
+
+//moment js 1h 1m 23sec countdown
